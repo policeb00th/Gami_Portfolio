8106f6c28727cc573d63e364424fd34238cde517
Diptanshu <sinha.diptanshu10@gmail.com>
2020-05-29 12:44:12 +0530

    Finished the introduction content ang alignment, will start with socieal media icons now

--- src/components/IntroductionContent.js
+++ src/components/IntroductionContent.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import Introduction from './Introduction';
 
 const IntroductionContent = () =>(
-    <div>
-        <h6> Sahil Gami</h6>
-        <h1>Website and ui/ux designer</h1>
-        <h1>3d artist</h1>
-        <p>UG student with a keen interest in designing world. I have been a part of multiple start-ups and chapters of my college with the sole intention to use and develop my skills further by providing the team to help them raise their potential reach.</p>
+    <div className="pagetext">
+        <p className="name_intro"> SAHIL GAMI</p>
+        <h1 className="intro_pink">Website and ui/ux designer</h1>
+        <h1 className="intro_pink">3d artist</h1>
+        <p className="intro_text">UG student with a keen interest in designing world. I have been a part of multiple start-ups and chapters of my college with the sole intention to use and develop my skills further by providing the team to help them raise their potential reach.</p>
    
     </div>
 )
