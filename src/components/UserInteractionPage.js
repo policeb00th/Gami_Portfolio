c32e21b0a21abcfdf96c1d1066aceacace235c05
Diptanshu <sinha.diptanshu10@gmail.com>
2020-06-24 16:13:07 +0530

    Adding loading to UI page

--- src/components/UserInteractionPage.js
+++ src/components/UserInteractionPage.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Footer from './Footer';
 import OtherPagesHeader from './OtherPagesHeader';
+import LazyLoad from 'react-lazyload';
+import Loading from './Loading';
 
 const UserInteractionPage = () => (
     <div>
-        <OtherPagesHeader/>
-        <p className="UI_Header">User <br/><span className="UI_small">Interaction</span></p>
+        <OtherPagesHeader />
+        <p className="UI_Header">User <br /><span className="UI_small">Interaction</span></p>
         <div className="image_container_main">
+
             <div className="image_container_left" >
                 <a href="https://www.behance.net/gallery/85147681/Architect-advertisement-web-design" target="_blank">
-                    <img className="image_style" src="/images/UserInteraction/architect.png" />
+                    <LazyLoad
+                        placeholder={Loading}>
+                        <img className="image_style" src="/images/UserInteraction/architect.png" />
+                    </LazyLoad>
                     <div className=" image_overlay_left">
                         <div className="image_text">
                             ARCHITECT PORTFOLIO
@@ -17,9 +23,13 @@ const UserInteractionPage = () => (
                     </div>
                 </a>
             </div>
+
             <div className="image_container_right" >
                 <a href="https://punar.in/" target="_blank">
-                    <img className="image_style" src="/images/UserInteraction/punar_healthcare.png" />
+                    <LazyLoad
+                        placeholder={Loading}>
+                        <img className="image_style" src="/images/UserInteraction/punar_healthcare.png" />
+                    </LazyLoad>
                     <div className=" image_overlay_right">
                         <div className="image_text">
                             PUNAR HEALTHCARE
@@ -31,17 +41,23 @@ const UserInteractionPage = () => (
         <div className="image_container_main">
             <div className="image_container_left" >
                 <a href="https://www.behance.net/gallery/85146453/Opportunity-%28Internship-job%29" target="_blank">
-                    <img className="image_style_left" src="/images/UserInteraction/Discovering_l_in_t.png" />
+                    <LazyLoad
+                        placeholder={Loading}>
+                        <img className="image_style_left" src="/images/UserInteraction/Discovering_l_in_t.png" />
+                    </LazyLoad>
                     <div className=" image_overlay_left">
                         <div className="image_text">
-                           C2C OPPORTUNITIES
+                            C2C OPPORTUNITIES
                     </div>
                     </div>
                 </a>
             </div>
             <div className="image_container_right" >
                 <a href="https://www.behance.net/gallery/86232445/Hackgrid-Hackathon-Website-Apple-Developers-Group" target="_blank">
-                    <img className="image_style_left" src="/images/UserInteraction/Hackgrid.png" />
+                    <LazyLoad
+                        placeholder={Loading}>
+                        <img className="image_style_left" src="/images/UserInteraction/Hackgrid.png" />
+                    </LazyLoad>
                     <div className=" image_overlay_right">
                         <div className="image_text">
                             HACKGRID
@@ -53,20 +69,26 @@ const UserInteractionPage = () => (
         <div className="image_container_main">
             <div className="image_container_left" >
                 <a href="https://www.behance.net/gallery/95649977/Login-Sign-up-concept-page" target="_blank">
-                    <img className="image_style_left" src="/images/UserInteraction/Login.png" />
+                    <LazyLoad
+                        placeholder={Loading}>
+                        <img className="image_style_left" src="/images/UserInteraction/Login.png" />
+                    </LazyLoad>
                     <div className=" image_overlay_left">
                         <div className="image_text">
-                            LOGIN PAGE<br/>CONCEPT
+                            LOGIN PAGE<br />CONCEPT
                     </div>
                     </div>
                 </a>
             </div>
             <div className="image_container_right" >
                 <a href="https://www.behance.net/gallery/89049843/Analytics-site-home-page" target="_blank">
-                    <img className="image_style_left" src="/images/UserInteraction/Tweak_company_landing.png" />
+                    <LazyLoad
+                        placeholder={Loading}>
+                        <img className="image_style_left" src="/images/UserInteraction/Tweak_company_landing.png" />
+                    </LazyLoad>
                     <div className=" image_overlay_right">
                         <div className="image_text">
-                           LANDING PAGE <br/> CONCEPT
+                            LANDING PAGE <br /> CONCEPT
                 </div>
                     </div>
                 </a>
