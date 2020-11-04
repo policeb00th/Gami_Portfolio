532e3ba6c2aeecda4693a6faff9cd459a376e308
Diptanshu <sinha.diptanshu10@gmail.com>
2020-11-04 15:58:59 +0530

    Minor Fix

--- src/components/AboutMeTextContainer.js
+++ src/components/AboutMeTextContainer.js
@@ -4,7 +4,7 @@ import AboutMeText from './AboutMeTextBox';
 
 const AboutMeTextContainer= () =>(
     <div className="about_container">
-        <AboutMeText pink_text="Working progress" hover_text="I tend to keep on iterating the concepts I use in every design 
+        <AboutMeText pink_text="Working process" hover_text="I tend to keep on iterating the concepts I use in every design 
         which helps me not to hold on to a single thinking process, making use of 3D software,animations, prototypes, illustrations
         minimalism,vintage look and just freestyling somethingis my way to go."/>
         <AboutMeText pink_text="What I do" hover_text="I have a good experience of around 2 years on poster making and user interaction 
